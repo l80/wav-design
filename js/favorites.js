42f16430ff70610a491fbc6563dc82791896fc4a
fedor.belov <fedor.belov@mail.ru>
2013-06-06 15:51:54 +0400

    VS-1354 favorites v1

---
+++ js/favorites.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    /* some custom settings */
+//    $('.iosSliderVertical').iosSliderVertical({
+//        snapToChildren: true,
+//        scrollbarHide: true,
+//        desktopClickDrag: true,
+//        scrollbarLocation: 'left',
+//        scrollbarWidth: '6px',
+////        scrollbarBackground: 'url(_img/some-img.png) repeat 0 0',
+//        scrollbarBorder: '1px solid #000',
+//        scrollbarMargin: '0 30px 16px 30px',
+//        scrollbarOpacity: '0.75'
+//    });
+});
+
+$(window).load(function() {
+    var myScroll = new iScroll('favorite-movies');
+//
+//    setTimeout(function () {
+//        myScroll.refresh();
+//    }, 100);
+});
