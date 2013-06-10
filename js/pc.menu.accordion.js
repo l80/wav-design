a1c39588c4ff1f40faf18c79f409bf50e3edfddc
fedor.belov <fedor.belov@mail.ru>
2013-06-10 11:41:01 +0400

    VS-1354 accordion version

--- js/pc.menu.accordion.js
+++ js/pc.menu.accordion.js
@@ -3,8 +3,22 @@ $(document).ready(function() {
 //        stowage: true
 //    });
 
+//    $('.opened-navigation .accordion').accordion({
+//        autoheight: false,
+//        collapsible: true
+//    });
+
+    //http://projects.timhuegdon.com/jquery-accordion/
     $('.opened-navigation .accordion').accordion({
-        autoheight: false,
-        collapsible: true
+        handle: ".handle", // Default: "h3"
+        panel: ".panel", // Default: ".panel"
+        speed: 500, // Default: 200
+//        easing: "easeInOutQuad", // Default "swing"
+        canOpenMultiple: true, // Default: false
+        canToggle: true, // Default: false
+        activeClassPanel: "open", // Default: "open"
+        activeClassLi: "active", // Default: "active"
+        lockedClass: "locked", // Default: "locked"
+        loadingClass: "loading" // Default: "loading"
     });
 });
