4011f327bc0a9001a56a9ee1f0bb12d0d15b29ce
fedor.belov <fedor.belov@mail.ru>
2013-06-17 15:51:23 +0400

    refactored

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 http_path = "/"
-css_dir = "scss"
+css_dir = "css/scss"
 sass_dir = "scss"
 images_dir = "img"
 http_images_path = "/img"
 
-output_style = :expanded # or :nested or :compact or :compressed
+#output_style = :expanded # or :nested or :compact or :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+line_comments = false
+sass_options = {:debug_info => true}
