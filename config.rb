2ff7ff9caa05e09fdf80c29b73c2e155ddf4c6ef
fedor.belov <fedor.belov@mail.ru>
2013-06-18 15:10:22 +0400

    added comments

--- config.rb
+++ config.rb
@@ -7,5 +7,5 @@ http_images_path = "/img"
 #output_style = :expanded # or :nested or :compact or :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+line_comments = true
 sass_options = {:debug_info => false, :force => false}
